c75895bd1584604ae28cf0b1919f71fd41e5e363
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2017-02-12 09:02:09 -0300

    app.example

--- app/Providers/ArangoProvider/Service/Provider.php
+++ app/Providers/ArangoProvider/Service/Provider.php
@@ -22,27 +22,18 @@ class Provider implements ServiceProviderInterface
             throw new \Exception("Connection settings for arango database is missing", 1);
         }
 
-        $db = [
-            'database' => $app['config']['arangodb']['database'],
-            'host' => sprintf(
-                'tcp://%s:%d',
-                $app['config']['arangodb']['host'],
-                $app['config']['arangodb']['port']
-            ),
-            'user' => $app['config']['arangodb']['user'],
-            'pass' => $app['config']['arangodb']['password']
-        ];
+        $host = sprintf('tcp://%s:%d', $app['config']['arangodb']['host'], $app['config']['arangodb']['port'] );
 
         $connectionOptions = array(
-            ArangoConnectionOptions::OPTION_DATABASE => $db['database'],
-            ArangoConnectionOptions::OPTION_ENDPOINT => $db['host'],
-            ArangoConnectionOptions::OPTION_AUTH_TYPE => 'Basic',
-            ArangoConnectionOptions::OPTION_AUTH_USER => $db['user'],
-            ArangoConnectionOptions::OPTION_AUTH_PASSWD => $db['pass'],
-            ArangoConnectionOptions::OPTION_CONNECTION => 'Close',
-            ArangoConnectionOptions::OPTION_TIMEOUT => 3,
-            ArangoConnectionOptions::OPTION_RECONNECT => true,
-            ArangoConnectionOptions::OPTION_CREATE => true,
+            ArangoConnectionOptions::OPTION_DATABASE    => $app['config']['arangodb']['database'],
+            ArangoConnectionOptions::OPTION_ENDPOINT    => $host,
+            ArangoConnectionOptions::OPTION_AUTH_TYPE   => $app['config']['arangodb']['auth'],
+            ArangoConnectionOptions::OPTION_AUTH_USER   => $app['config']['arangodb']['user'],
+            ArangoConnectionOptions::OPTION_AUTH_PASSWD => $app['config']['arangodb']['password'],
+            ArangoConnectionOptions::OPTION_CONNECTION  => $app['config']['arangodb']['connection'],
+            ArangoConnectionOptions::OPTION_TIMEOUT     => $app['config']['arangodb']['timeout'],
+            ArangoConnectionOptions::OPTION_RECONNECT   => $app['config']['arangodb']['reconnect'],
+            ArangoConnectionOptions::OPTION_CREATE      => $app['config']['arangodb']['create'],
             ArangoConnectionOptions::OPTION_UPDATE_POLICY => ArangoUpdatePolicy::LAST,
         );
 
@@ -52,6 +43,6 @@ class Provider implements ServiceProviderInterface
             return new Factory(new ArangoConnection($connectionOptions));
         };
 
-        unset($db);
+        unset($host);
     }
 }
